aa6d93e6b4f8c7ca6d323f3885b29290710dbc37
aunnnn <aun.wirawit@gmail.com>
2017-12-23 22:15:31 +0700

    Try dirty-hack for overlay!

--- pages/index.js
+++ pages/index.js
@@ -16,9 +16,26 @@ export default class Index extends Component {
     const coindarData = await coindar('btc')
     const chartResponse = await getCharts('bitcoin')
     const listOfTickers = await getListOfTickers()
-  
 
     const transformed = transformData.prices(chartResponse)
+
+    const coindarDict = {};
+    coindarData.forEach(d => {
+      coindarDict[d.start_date.format('DD MMM YYYY')] = d
+    })
+
+    const highest = Math.max(...transformed.y)*0.75
+
+    let count = 0
+    const dataYForEvents = transformed.x.map((d) => {
+      const event = coindarDict[d]
+      if (!event) { return 0 }
+      count++
+      return highest
+    })    
+
+    // alert('Detect: ' + count + 'out of ' + coindarData.length)
+
     const cmcData = {
       labels: transformed.x,
       datasets: [
@@ -42,8 +59,25 @@ export default class Index extends Component {
           pointRadius: 1,
           pointHitRadius: 10,
           data: transformed.y
+        },
+        {
+          label: 'Events',
+          fill: false,
+          lineTension: 0.05,
+          backgroundColor: 'rgba(255,0,0,0.7)',
+          borderColor: 'rgba(255,0,0,1)',
+          pointBorderColor: 'rgba(255,0,0,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 0.5,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: dataYForEvents,
         }
-      ]
+      ],      
     }
     this.setState({ coindarData, cmcData, listOfTickers })
   }
@@ -63,6 +97,21 @@ export default class Index extends Component {
       <div>
         <h1>Test API Page</h1>
         <Line data={cmcData} />
+        <div>
+          {
+            coindarData.map((d, i) => {
+              return (
+                <div key={`d-${i}`}>
+                  <div>
+                    <h4>{d.caption}
+                      <em><span>  - {d.start_date.format('DD MMM YYYY')}</span></em>
+                    </h4>
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
       </div>
     )
   }
--- utils/transformData.js
+++ utils/transformData.js
@@ -2,8 +2,11 @@ import moment from 'moment'
 
 export default {
   prices(cmcData) {
-    const xPoints = cmcData.price_usd.map(item => moment(item[0]).format('dd MM'))
-    const yPoints = cmcData.price_usd.map(item => item[1])
+    const filtered = cmcData.price_usd.filter(item => {
+      return moment(item[0]).format('YYYY') === '2017'
+    }) 
+    const xPoints = filtered.map(item => moment(item[0]).format('DD MMM YYYY'))
+    const yPoints = filtered.map(item => item[1])
     return {
       x: xPoints,
       y: yPoints
