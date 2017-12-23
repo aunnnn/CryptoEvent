eba678afed0c39efcb2f57a32cf7653dbb59e293
Jirat Onaree <jirat.onaree@gmail.com>
2017-12-23 23:05:36 +0700

    add year

--- utils/transformData.js
+++ utils/transformData.js
@@ -2,7 +2,7 @@ import moment from 'moment'
 
 export default {
   prices(cmcData) {
-    const xPoints = cmcData.price_usd.map(item => moment(item[0]).format('dd MM'))
+    const xPoints = cmcData.price_usd.map(item => moment(item[0]).format('DD MMM YYYY'))
     const yPoints = cmcData.price_usd.map(item => item[1])
     return {
       x: xPoints,
