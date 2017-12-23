d3cc2980ec643cc79d5efd21e777335db823a179
aunnnn <aun.wirawit@gmail.com>
2017-12-23 19:02:01 +0700

    Add coinmarketcap API

--- services/coinmarketcap.js
+++ services/coinmarketcap.js
@@ -1,9 +1,13 @@
 import fetch from 'isomorphic-fetch'
 
-const main = async () => {
-  const res = await fetch('https://api.coinmarketcap.com/v1/ticker/?limit=10')
+export const getListOfTickers = async () => {
+  const res = await fetch('https://api.coinmarketcap.com/v1/ticker/?limit=100')
   const json = await res.json()
   return json
 }
 
-export default main;
+export const getCharts = async (id) => {
+  const res = await fetch(`https://graphs.coinmarketcap.com/currencies/${id}/`)
+  const price_usd = (await res.json()).price_usd
+  return price_usd
+}
